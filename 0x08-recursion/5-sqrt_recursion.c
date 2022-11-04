35c5007dde00b7471b74077d87f3235406dec292
MayowaShoyinka <47593314+MayowaShoyinka@users.noreply.github.com>
2022-11-04 05:08:55 +0100

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,17 +2,7 @@
 /**
  *_evaluate - evaluate function sqrt
  *@i: interger
- *@n: interger#include "main.h"
-/**
- *_evaluate - evaluate function sqrt
- *@i: interger
  *@n: interger
- *Return: evaluate sqrt
- */
-
-int _evaluate(int i, int n)
-{
-/*Evaluate function*/
  *Return: evaluate sqrt
  */
 
